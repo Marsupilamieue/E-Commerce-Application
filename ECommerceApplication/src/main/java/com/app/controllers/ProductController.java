c83739b6cc4e335285fe1b30d6bd8f173dfaeaf0
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2025-02-12 19:20:41 +0700

    Add GetProductByBrand Endpoint.

--- ECommerceApplication/src/main/java/com/app/controllers/ProductController.java
+++ ECommerceApplication/src/main/java/com/app/controllers/ProductController.java
@@ -65,6 +65,19 @@ public class ProductController {
 
 		return new ResponseEntity<ProductResponse>(productResponse, HttpStatus.FOUND);
 	}
+
+	@GetMapping("/public/brand/{brandName}/products")
+	public ResponseEntity<ProductResponse> getProductsByBrand(@PathVariable String brandName,
+			 @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+			 @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+			 @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_PRODUCTS_BY, required = false) String sortBy,
+			 @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
+
+		ProductResponse productResponse = productService.searchByBrand(brandName, pageNumber, pageSize, sortBy,
+				sortOrder);
+
+		return new ResponseEntity<ProductResponse>(productResponse, HttpStatus.FOUND);
+	}
 	
 	@GetMapping("/public/products/keyword/{keyword}")
 	public ResponseEntity<ProductResponse> getProductsByKeyword(@PathVariable String keyword,
